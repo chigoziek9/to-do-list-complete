98b0e825f4f3e88bc5f301517ff6c44845bf579d
Nony <nonybrighto@gmail.com>
2025-05-30 00:16:31 +0100

    merger

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <h1 className="text-3xl sm:text-4xl font-bold text-center py-4">
         MY To Do List
       </h1>
+      <h2>gu nnn</h2>
       <div className="p-2 sm:p-4 max-w-xl mx-auto">
         <AddTask />
       </div>
